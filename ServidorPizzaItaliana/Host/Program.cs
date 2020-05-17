9aa008d6e8ef727b651bb975000cf06679b4c1ac
Javier Juarez <javierjuarez.jj28@gmail.com>
2020-05-17 01:23:15 -0500

    sadasd
    asdasd

---
+++ ServidorPizzaItaliana/Host/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Sockets;
+using System.ServiceModel;
+
+namespace Host
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(ServidrorPizzaItaliana.ServicioPizzaItaliana)))
+            {
+                try
+                {
+                    host.Open();
+                    Console.WriteLine("El servidor está arriba");
+                    Console.ReadLine();
+                }
+                catch (SocketException)
+                {
+                    Console.WriteLine("Error de conexion");
+                }
+            }
+        }
+
+    }
+}
---
+++ ServidorPizzaItaliana/ServidrorPizzaItaliana/IServicioPizzaItaliana.cs
@@ -0,0 +1,38 @@
+﻿using AccesoBD2;
+using System.ServiceModel;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+
+namespace ServidrorPizzaItaliana
+{
+   
+    [ServiceContract(CallbackContract = typeof(ICalculatorServiceCallback))]
+    public interface IServicioPizzaItaliana
+    {
+        [OperationContract(IsOneWay = true)]
+        void IniciarSesion(string nombreUsuario, string contraseña);
+        [OperationContract(IsOneWay = true)]
+        void CerrarSesion(string nombreUsuario);
+        [OperationContract(IsOneWay = true)]
+        void GuardarCuentaUsuario(CuentaUsuario cuentaUsuario);
+        [OperationContract(IsOneWay = true)]
+        void ModificarCuentaUsuario(CuentaUsuario cuentaUsuario);
+
+    }
+
+    /// <summary>
+    /// La interfazICalculatorServiceCallback se encarga de especificar los
+    /// metodos que un cliente puede utilizar del servidor.
+    /// Contiene todas las funcionalidades que requiere el cliente.
+    /// </summary>
+    [ServiceContract]
+    public interface ICalculatorServiceCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void Respuesta(string mensaje);
+        [OperationContract(IsOneWay = true)]
+        void DevuelveCuenta(CuentaUsuario cuenta);
+
+    }
+}
